4413892812e45b275b6cd263d54c2ed3dd13d906
ThreshMain <martinkos007@gmail.com>
2021-11-01 15:00:22 +0100

    Added ascii profile image

--- install.sh
+++ install.sh
@@ -6,9 +6,16 @@ distro=$(awk -F '=' 'FNR == 3 { print $2 }' /etc/os-release)
 echo "Installing jq"
 
 if [[ distro -eq "arch" ]]; then
+	git clone https://aur.archlinux.org/ascii-image-converter-git.git
+	cd ascii-image-converter-git/
+	makepkg -si
+	cd ..
+	rm -rf ascii-image-converter-git
 	sudo pacman -Syu jq
 elif [[ distro -eq "ubuntu" ]];  then
-	sudo apt install jq
+	echo 'deb [trusted=yes] https://apt.fury.io/ascii-image-converter/ /' | sudo tee /etc/apt/sources.list.d/ascii-image-converter.list
+	sudo apt update
+	sudo apt install jq ascii-image-converter
 fi
 
 chmod +x gitfetch
